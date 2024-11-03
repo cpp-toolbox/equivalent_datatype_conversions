1c7798eb397d511c230fe05d6f4660f9161a09bd
cuppajoeman <ccn@cuppajoeman.com>
2024-11-03 01:26:35 -0500

    feat: add conversion of vector of types

--- equivalent_datatype_conversions.cpp
+++ equivalent_datatype_conversions.cpp
@@ -8,6 +8,15 @@ JPH::Vec3 convert_vec3_from_glm_to_jolt(glm::vec3 input_vector) {
     return {input_vector.x, input_vector.y, input_vector.z};
 }
 
+std::vector<JPH::Vec3> convert_vec3_vector_from_glm_to_jolt(const std::vector<glm::vec3> &input_vectors) {
+    std::vector<JPH::Vec3> output_vectors;
+    output_vectors.reserve(input_vectors.size());
+    for (const auto &glm_vec : input_vectors) {
+        output_vectors.push_back(convert_vec3_from_glm_to_jolt(glm_vec));
+    }
+    return output_vectors;
+}
+
 glm::mat4 convert_mat4_from_jolt_to_glm(JPH::Mat44 input_matrix) {
     glm::mat4 glm_mat;
     for (int col = 0; col < 4; col++) {
--- equivalent_datatype_conversions.hpp
+++ equivalent_datatype_conversions.hpp
@@ -5,9 +5,10 @@
 #include "Jolt/Jolt.h"
 
 JPH::Vec3 convert_vec3_from_glm_to_jolt(glm::vec3 input_vector);
+std::vector<JPH::Vec3> convert_vec3_vector_from_glm_to_jolt(const std::vector<glm::vec3> &input_vectors);
 
 glm::vec3 convert_vec3_from_jolt_to_glm(JPH::Vec3 input_vector);
 
 glm::mat4 convert_mat4_from_jolt_to_glm(JPH::Mat44 input_matrix);
 
-#endif //EQUIVALENT_DATATYPE_CONVERSIONS_HPP
+#endif // EQUIVALENT_DATATYPE_CONVERSIONS_HPP
